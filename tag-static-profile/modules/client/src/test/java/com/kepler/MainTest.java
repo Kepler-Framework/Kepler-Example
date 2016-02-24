21247c3c6d8512e05031cfd084849e08b408412a
shenjiawei <shenjiawei@didichuxing.com>
2016-02-24 15:49:06 +0800

    tag profile

--- tag-static-profile/modules/client/src/test/java/com/kepler/MainTest.java
+++ tag-static-profile/modules/client/src/test/java/com/kepler/MainTest.java
@@ -11,10 +11,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class MainTest {
 
 	@Autowired
-	private TestApp1 test;
+	private TestApp1 test1;
+
+	@Autowired
+	private TestApp2 test2;
 
 	@Test
-	public void test() throws Exception {
-		System.out.println(this.test.test("kepler"));
+	public void test1() throws Exception {
+		System.out.println(this.test1.test("kepler"));
+	}
+
+	@Test
+	public void test2() throws Exception {
+		System.out.println(this.test2.test("kepler"));
 	}
 }
--- tag-static-profile/modules/service/src/main/java/com/kepler/impl/TestApp1Impl.java
+++ tag-static-profile/modules/service/src/main/java/com/kepler/impl/TestApp1Impl.java
@@ -2,12 +2,11 @@ package com.kepler.impl;
 
 import com.kepler.TestApp1;
 import com.kepler.annotation.Autowired;
-import com.kepler.host.Host;
 
 @Autowired
-public class TestAppImpl implements TestApp1 {
+public class TestApp1Impl implements TestApp1 {
 
 	public String test(String kepler) throws Exception {
-		return "Hello world " + kepler + " " + Host.TAG_VAL;
+		return "Hello world 1 " + kepler;
 	}
 }
