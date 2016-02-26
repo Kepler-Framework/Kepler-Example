b716d061c3d8b5db3340c796f7b73579f24a44eb
shenjiawei <shenjiawei@didichuxing.com>
2016-02-26 11:08:42 +0800

    serial smart

---
+++ serial-smart/modules/interface/src/main/java/com/kepler/TestApp1.java
@@ -0,0 +1,8 @@
+package com.kepler;
+import com.kepler.annotation.Service;
+
+@Service(version = "0.0.1-serial")
+public interface TestApp1 {
+
+	public String test(String kepler) throws Exception;
+}
