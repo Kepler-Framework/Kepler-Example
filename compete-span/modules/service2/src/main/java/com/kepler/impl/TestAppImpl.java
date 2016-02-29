d1df9b7234469076a2ab1adda085429b28e65435
shenjiawei <shenjiawei@didichuxing.com>
2016-02-29 12:15:03 +0800

    compete span

---
+++ compete-span/modules/service2/src/main/java/com/kepler/impl/TestAppImpl.java
@@ -0,0 +1,13 @@
+package com.kepler.impl;
+
+import com.kepler.TestApp;
+import com.kepler.annotation.Autowired;
+
+@Autowired
+public class TestAppImpl implements TestApp {
+
+	public String test(String kepler) throws Exception {
+		Thread.sleep(1000);
+		return "Hello world 2 " + kepler;
+	}
+}
---
+++ compete-span/modules/service2/target/srv-compete-service2/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash run.sh 1 compete-service-kepler.jar
