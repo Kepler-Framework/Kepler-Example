fb431ed03240df821415804b92a6213dca0ae84c
shenjiawei <shenjiawei@didichuxing.com>
2016-08-18 23:25:07 +0800

    修正性能压测用例

--- pressure/src/main/java/main/Client.java
+++ pressure/src/main/java/main/Client.java
@@ -8,10 +8,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
 
 import com.alibaba.dubbo.demo.DemoService;
 
 public class Client {
+
+	/**
+	 * 单次循环
+	 */
+	private static final int CIRCLE = Integer.valueOf(System.getProperty("circle", "10000"));
 
 	private static String build(int length) {
 		StringBuffer buffer = new StringBuffer();
@@ -22,28 +28,33 @@ public class Client {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Input the params: (server type: 1:Dubbo or 2:Kepler) (request size(KB)) (thread number) (circle times) >> ");
+		System.out.println("Input the params: (server type: 1:Dubbo or 2:Kepler) (request size(KB)) (thread number) (circle times) (warming) >> ");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		String[] params = input.readLine().split(" ");
 		int client = Integer.valueOf(params[0]);
 		int request = Integer.valueOf(params[1]);
 		int threads = Integer.valueOf(params[2]);
 		int circles = Integer.valueOf(params[3]);
+		Assert.isTrue(circles >= Client.CIRCLE * 2);
+		Assert.isTrue(circles % Client.CIRCLE == 0);
 		String buffer = Client.build(request);
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(client == 1 ? "dubbo-consumer.xml" : "kepler-client.xml");
 		context.start();
 		// 获取远程服务代理
 		DemoService demoService = (DemoService) context.getBean("demoService");
 		// 预热
-		for (int index = 0; index < circles; index++) {
+		for (int index = 0; index < circles / Client.CIRCLE; index++) {
 			demoService.sayHello(buffer);
 		}
+		System.gc();
+		Thread.sleep(5000);
+		System.out.println("---------------------[START]---------------------");
 		AtomicLong counter = new AtomicLong(circles);
 		InvokerRunnable invoker = new InvokerRunnable(demoService, counter, buffer);
 		ExecutorService executor = Executors.newFixedThreadPool(threads);
 		// 计时
 		long start = System.nanoTime();
-		for (int index = 0; index < circles; index++) {
+		for (int index = 0; index < circles / Client.CIRCLE; index++) {
 			executor.execute(invoker);
 		}
 		synchronized (counter) {
@@ -78,8 +89,10 @@ public class Client {
 		}
 
 		public void run() {
-			this.demoService.sayHello(this.buffer);
-			this.counter.decrementAndGet();
+			for (int index = 0; index < Client.CIRCLE; index++) {
+				this.demoService.sayHello(this.buffer);
+			}
+			this.counter.addAndGet(-Client.CIRCLE);
 			if (this.counter.get() == 0) {
 				synchronized (this.counter) {
 					this.counter.notifyAll();
